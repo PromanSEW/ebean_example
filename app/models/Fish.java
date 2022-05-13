71a453d2df0730556378b00b065a73da3f480937
Roman Parshikov <promansew@gmail.com>
2022-05-13 15:00:08 +0800

    Delete all unused code

--- app/models/Fish.java
+++ app/models/Fish.java
@@ -1,20 +1,15 @@
 package models;
 
-import io.ebean.Finder;
 import io.ebean.Model;
 
-import javax.annotation.Nonnull;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table(name = "fishes")
 public class Fish extends Model {
-
-	private static final Finder<Integer, Fish> finder = new Finder<>(Fish.class);
 
 	@Id
 	public int id;
@@ -22,41 +17,10 @@ public class Fish extends Model {
 	public String name;
 
 	public Fish(String name) {
-		this.name = name;
+		setName(name);
 	}
 
 	private void setName(String name) {
 		this.name = name;
-	}
-
-	/** Все рыбы */
-	@Nonnull
-	public static List<Fish> all() {
-		return finder.query().orderById(true).findList();
-	}
-
-	public static Fish byId(int id) {
-		return finder.byId(id);
-	}
-
-	/** @return Отсортировнный по имени список рыб */
-	@Nonnull
-	public static List<Fish> sorted() {
-		return finder.query().order("name").findList();
-	}
-
-	public static boolean update(int id, String name) {
-		Fish fish = Fish.byId(id);
-		if (fish == null) return false;
-		fish.setName(name);
-		fish.update();
-		return true;
-	}
-
-	/** Добавить рыб при установке сервера */
-	public static void addFishes(@Nonnull List<String> fishes) {
-		for (String fish : fishes) {
-			new Fish(fish).save();
-		}
 	}
 }
